78f904d688576600f563b4659e39fcb6ff8cc93c
tt9297979-droid <tt9297979@gmail.com>
2026-03-17 20:43:49 +0700

    Update verifySlip.js

--- api/verifySlip.js
+++ api/verifySlip.js
@@ -1,12 +1,23 @@
 export default async function handler(req,res){
 
-let {amount, ref} = req.query
+const {slip} = req.body
 
-// จำลอง AI ตรวจสลิป
-if(amount == 99){
+const r = await fetch("https://developer.easyslip.com/api/v1/verify",{
+  method:"POST",
+  headers:{
+    "Content-Type":"application/json",
+    "Authorization":"Bearer YOUR_API_KEY"
+  },
+  body: JSON.stringify({
+    slip_image: slip
+  })
+})
+
+const data = await r.json()
+
+if(data.data.amount == 99){
   return res.json({valid:true})
 }
 
 res.json({valid:false})
-
 }
